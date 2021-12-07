395c14eb5dc9194fd7538f2b00a60db1f7115558
aravindheroku <aravind.puk@gmail.com>
2021-12-07 21:36:00 +0530

    update setting

--- myfamily/settings.py
+++ myfamily/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--=#8p_+*kiy-(a-dmt9f)lua+9l_7sl0n1^cf@$p)okzzh$f_@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
